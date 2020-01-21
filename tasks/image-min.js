edf140a7549d152517a632be2acaa91dfd4e80d5
dsymonov <dsymonov@justcoded.co>
2020-01-21 17:42:26 +0200

    Update image-min

--- tasks/image-min.js
+++ tasks/image-min.js
@@ -7,37 +7,27 @@ const gulp = require('gulp');
 const newer = require('gulp-newer');
 const imagemin = require('gulp-imagemin');
 
-module.exports = function(options) {
+module.exports = function (options) {
   return () => {
     return gulp
       .src('./assets/**/*.{gif,png,jpg,jpeg,svg}')
       .pipe(newer('./assets/**/*.{gif,png,jpg,jpeg,svg}'))
       .pipe(
         imagemin([
-          imagemin.gifsicle({
-            interlaced: true,
-          }),
-          imagemin.jpegtran({
-            progressive: true,
-          }),
-          imagemin.optipng({
-            optimizationLevel: 5,
-          }),
+          imagemin.gifsicle({ interlaced: true }),
+          imagemin.mozjpeg({ quality: 75, progressive: true }),
+          imagemin.optipng({ optimizationLevel: 5 }),
           imagemin.svgo({
             plugins: [
               {
                 removeViewBox: false,
                 collapseGroups: true,
                 removeComments: true,
-              },
-            ],
-          }),
+              }
+            ]
+          })
         ]),
       )
-      .pipe(
-        gulp.dest(function setFile(file) {
-          return file.base;
-        }),
-      );
+      .pipe(gulp.dest('./assets'));
   };
 };
